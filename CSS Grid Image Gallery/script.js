9da99ca351cd7d9ad98d36168170d7d2024bc5d1
ElaMoscicka <elakorzeniowska@interia.pl>
2018-04-23 21:16:36 +0200

    Create script.js

---
+++ CSS Grid Image Gallery/script.js
@@ -0,0 +1,43 @@
+const gallery = document.querySelector('.gallery');
+const overlay = document.querySelector('.overlay');
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
+
+/*generating HTML*/
+function generateHTML([h, v]) {
+    return `
+    <div class="item h${h} v${v}">
+      <img src="images/${randomNumber(12)}.jpg">
+      <div class="item__overlay">
+        <button>View →</button>
+      </div>
+    </div>
+  `;
+}
+
+function randomNumber(limit) {
+    return Math.floor(Math.random() * limit) + 1;
+}
+
+function handleClick(e) {
+    const src = e.currentTarget.querySelector('img').src;
+    overlayImage.src = src;
+    overlay.classList.add('open'); /*open overlay*/
+  }
+
+  /*close overlay*/
+function close() {
+    overlay.classList.remove('open');
+}
+
+/*bunch of random arrays*/
+const digits = Array.from({ length: 50 }, () => [randomNumber(4), randomNumber(4)]).concat([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]);
+
+const html = digits.map(generateHTML).join('');
+gallery.innerHTML = html;
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => item.addEventListener('click', handleClick));
+
+overlayClose.addEventListener('click', close);
